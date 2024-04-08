5c32ecd6470c06f1b6303d278d6e8a0042749644
Massimo Albanese <33076159+madlitch@users.noreply.github.com>
2024-04-07 20:39:30 -0400

    - dynamic project loading from projects.json - toned stuff down - added home page - added more information in about - changed to vertical scroll - added proper single page navigation - updated bootstrap - changed resume to just file - updated site description

--- app/constants.py
+++ app/constants.py
@@ -14,3 +14,5 @@ MAINTENANCE = os.getenv('MAINTENANCE')
 INSTAGRAM_LINK = 'https://instagram.com/madpixelsort'
 TWITTER_LINK = 'https://twitter.com/madpixelsort'
 YOUTUBE_LINK = 'https://www.youtube.com/channel/UCMGddKLu8RNtuAdqdBYb9SA'
+
+
