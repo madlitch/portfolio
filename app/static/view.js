24a53e81d5e4ce21b0c6c3e1bddb7880882c575b
Massimo Albanese <33076159+madlitch@users.noreply.github.com>
2024-09-18 17:54:25 -0400

    update theme update resume

--- app/static/view.js
+++ app/static/view.js
@@ -2,7 +2,7 @@ const src = "/static/images/";
 
 function checkPathAndSwitchTab() {
     const path = window.location.pathname;
-    const tab = path.split('/')[1]; // Assuming URL structure is like site.com/tabName
+    const tab = path.split('/')[1];
     selectTab(tab);
 }
 
@@ -36,7 +36,7 @@ window.onpopstate = function (event) {
     }
 };
 
-async function switchProjects(tag, button) {
+async function switchWorkFilter(tag, button) {
     const underline_highlights = document.getElementsByClassName('underline-highlight');
     const projects = document.getElementsByClassName('project');
 
@@ -110,77 +110,3 @@ function start() {
         });
     });
 }
-
-
-// let isThrottled = false;
-// const throttleDuration = 700; // Adjust the duration based on sensitivity preference
-//
-// function throttleChangeTab(direction) {
-//     if (!isThrottled) {
-//         isThrottled = true;
-//         changeTabOnScroll(direction);
-//
-//         setTimeout(() => {
-//             isThrottled = false;
-//         }, throttleDuration);
-//     }
-// }
-//
-//
-//
-// // let lastScrollPosition = 0;
-
-// window.addEventListener('wheel', (event) => {
-//     const direction = event.deltaY < 0 ? 'up' : 'down';
-//     throttleChangeTab(direction);
-// });
-
-// const tabs = ['home', 'about', 'projects']; // Ensure this matches the order of your tabs
-// let currentTabIndex = 0; // Start with the first tab
-//
-// function changeTabOnScroll(direction) {
-//     if (direction === 'down') {
-//         currentTabIndex = Math.min(currentTabIndex + 1, tabs.length - 1);
-//     } else if (direction === 'up') {
-//         currentTabIndex = Math.max(currentTabIndex - 1, 0);
-//     }
-//     switchTab(tabs[currentTabIndex]);
-// }
-
-// let accumulatedScrollY = 0;
-// const scrollThreshold = 600; // Adjust based on sensitivity preference
-//
-// function accumulateScrollDistance(deltaY) {
-//     accumulatedScrollY += deltaY;
-//
-//     if (Math.abs(accumulatedScrollY) > scrollThreshold) {
-//         const direction = accumulatedScrollY < 0 ? 'up' : 'down';
-//         changeTabOnScroll(direction);
-//         // Reset accumulated scroll after switching tabs
-//         accumulatedScrollY = 0;
-//     }
-// }
-//
-// document.querySelector('.scroll-container').addEventListener('wheel', (event) => {
-//     // Use event.deltaY for wheel scroll distance
-//     accumulateScrollDistance(event.deltaY);
-// });
-//
-// let touchStartY = 0;
-// let touchEndY = 0;
-//
-// window.addEventListener('touchstart', (event) => {
-//     touchStartY = event.touches[0].clientY;
-// }, false);
-//
-// window.addEventListener('touchend', (event) => {
-//     // Calculate the vertical distance moved
-//     let deltaY = touchStartY - touchEndY;
-//     accumulateScrollDistance(deltaY);
-//     // Reset touchEndY for the next touch sequence
-//     touchEndY = 0;
-// }, false);
-//
-// window.addEventListener('touchmove', (event) => {
-//     touchEndY = event.changedTouches[0].clientY;
-// }, false);
